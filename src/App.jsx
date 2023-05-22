585d1d3dc2837651eaa9d5d11b724f4b0f10ec55
Abdur-P-Rahim <bspirahim@gmail.com>
2023-05-22 17:43:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-bold'>Vite + React</h1>
+      <h1 className='text-6xl font-bold'>Vite + React</h1>
+      <button className="btn">Button</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+      
+    </>
+  )
+}
+
+export default App
