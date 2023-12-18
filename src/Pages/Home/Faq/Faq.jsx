5aea60fe65c9281e87b9022171abaf865e151d61
Abdur-P-Rahim <bspirahim@gmail.com>
2023-12-18 23:42:18 +0600

    image updated

--- src/Pages/Home/Faq/Faq.jsx
+++ src/Pages/Home/Faq/Faq.jsx
@@ -65,7 +65,7 @@ const Faq = () => {
 
                 </div>
                 <div>
-                    <img src="../../../../public/icon/faq-3.jpg" alt="" />
+                    <img src="/icon/faq-3.jpg" alt="" />
                 </div>
             </div>
         </div>
--- src/Pages/Home/PhotoGallery.jsx
+++ src/Pages/Home/PhotoGallery.jsx
@@ -5,26 +5,6 @@ import PhotoCard from '../../Shared/PhotoCard/PhotoCard';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const PhotoGallery = () => {
-    //     const {id} = useParams()
-    //     const [photoIds, setPhotoIds] = useState([]);
-
-    //   useEffect(() => {
-    //     const fetchPhotoIds = async () => {
-    //       try {
-    //         const response = await fetch(`https://wonder-toy-server-one.vercel.app/altoys/${_id}`); // Replace with your actual endpoint
-    //         const data = await response.json();
-    //         setPhotoIds(data.photoIds);
-    //       } catch (error) {
-    //         console.error('Error fetching photo IDs:', error);
-    //       }
-    //     };
-
-    //     fetchPhotoIds();
-    //   }, []); 
-
-
-
-
 
 
     useEffect(() => {
@@ -61,48 +41,96 @@ const PhotoGallery = () => {
                         <div className='w-full h-fit group'>
                             <div className='relative overflow-hidden'>
                                 <img class="h-auto max-w-full rounded-lg" src='https://img.freepik.com/premium-photo/knitted-stuffed-toys-amigurumi-handcrafted-crochet-hobby_637279-1401.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais' alt="" />
-                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300'>
-                                    <button className='bg-rose-900/30 text-white py-2 px-5'>Open</button>
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/30 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-
-
-
-
-
-
-
-
                     <div data-aos="fade-right">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-2.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-2.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-right">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-4.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-4.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="grid gap-4">
                     <div data-aos="fade-down">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-5.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-5.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-down">
-                        <img className="h-auto max-w-full rounded-lg" src="https://img.freepik.com/free-photo/still-life-crochet-plushies_23-2151091205.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='https://img.freepik.com/free-photo/still-life-crochet-plushies_23-2151091205.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-down">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-6.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-6.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="grid gap-4">
                     <div data-aos="fade-up">
-                        <img className="h-auto max-w-full rounded-lg" src="https://img.freepik.com/free-photo/still-life-crochet-plushies_23-2151091198.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='https://img.freepik.com/free-photo/still-life-crochet-plushies_23-2151091198.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-up">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-7.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-7.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-up">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-8.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-8.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -110,13 +138,34 @@ const PhotoGallery = () => {
 
                 <div className="grid gap-4">
                     <div data-aos="fade-left">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-1.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-1.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-left">
-                        <img className="h-auto max-w-full rounded-lg" src="https://img.freepik.com/free-photo/fluffy-toy-texture-close-up_23-2149686878.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='https://img.freepik.com/free-photo/fluffy-toy-texture-close-up_23-2149686878.jpg?size=626&ext=jpg&ga=GA1.1.1198683788.1701365080&semt=ais' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div data-aos="fade-left">
-                        <img className="h-auto max-w-full rounded-lg" src="../../../public/photoGallery/img-3.jpg" alt="" />
+                    <div className='w-full h-fit group'>
+                            <div className='relative overflow-hidden'>
+                                <img class="h-auto max-w-full rounded-lg" src='/photoGallery/img-3.jpg' alt="" />
+                                <div className='absolute h-full w-full bg-black/30 flex items-center justify-center -bottom-10 group-hover:bottom-0  opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-lg'>
+                                    <button className='bg-rose-900/50 rounded-tl-lg rounded-br-lg text-white py-2 px-5'>Open</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
